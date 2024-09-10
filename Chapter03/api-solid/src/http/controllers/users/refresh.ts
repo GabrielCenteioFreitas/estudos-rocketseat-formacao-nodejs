969582aa82389fc93e8ea8e51ad24e05239bb735
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-09-10 01:14:36 -0300

    Capítulo 3 - Aula 07

---
+++ Chapter03/api-solid/src/http/controllers/users/refresh.ts
@@ -0,0 +1,42 @@
+import { FastifyReply, FastifyRequest } from "fastify"
+
+export async function refresh(req: FastifyRequest, res: FastifyReply) {
+  await req.jwtVerify({ onlyCookie: true })
+
+  const { sub, role } = req.user
+
+  const token = await res.jwtSign(
+    {
+      role,
+    },
+    {
+      sign: {
+        sub,
+      },
+    },
+  )
+
+  const refreshToken = await res.jwtSign(
+    {
+      role,
+    },
+    {
+      sign: {
+        sub,
+        expiresIn: '7d',
+      },
+    },
+  )
+
+  return res
+    .setCookie('refreshToken', refreshToken, {
+      path: '/',
+      secure: true,
+      sameSite: true,
+      httpOnly: true,
+    })
+    .status(200)
+    .send({
+      token,
+    })
+}
