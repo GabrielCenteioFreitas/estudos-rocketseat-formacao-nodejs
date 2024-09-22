7139320014e6a9c0a3b7320c6f3cdef5179d8a23
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-09-22 16:55:23 -0300

    Capítulo 5 - Aula 05 - Parte 2/2

--- Chapter05/nest-clean/src/infra/database/database.module.ts
+++ Chapter05/nest-clean/src/infra/database/database.module.ts
@@ -18,8 +18,10 @@ import { PrismaQuestionsRepository } from './prisma/repositories/prisma-question
 import { PrismaStudentsRepository } from './prisma/repositories/prisma-students-repository';
 import { PrismaNotificationsRepository } from './prisma/repositories/prisma-notifications-repository';
 import { NotificationsRepository } from '@/domain/notification/application/repositories/notifications-repository';
+import { CacheModule } from '../cache/cache.module';
 
 @Module({
+  imports: [CacheModule],
   providers: [
     PrismaService,
     {
